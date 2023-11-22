bc59a538a805dc0ea5015434bc08f99b3cf0903a
Mason Kline <mkline13@gmail.com>
2023-11-21 17:01:14 -0800

    added page warnings, cleaned up styling a bit

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -3,7 +3,12 @@
 export default function dashboard(req, res) {
     const data = {
         pokes: [],
-        feed: []
+        feed: [],
+        page_alerts: [
+            {message: 'Hello world!', severity: 'info'},
+            {message: 'Uh-oh, world!', severity: 'warning'},
+            {message: 'Goodbye world!', severity: 'critical'}
+        ]
     }
     res.render('dashboard', data);
 }
